0da6cee1210497e590ed5f28ffb53d2552c7cbee
Sourodeep Saha <sourodeepsaha555@gmail.com>
2026-09-08 17:33:45 +0530

    Revert "Updated the register button once the user logs in and also created my profile section"

--- src/app/profile/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
-
-export default function ProfileLayout({ children }: { children: React.ReactNode }) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
-}
